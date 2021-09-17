8c8721debe912cb28184d67a4b2e11686062d04c
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-09-18 00:15:29 +0300

    установила сервер джейсон и аксиос

---
+++ src/servises/contacts-api.js
@@ -0,0 +1,15 @@
+//import axios from 'axios';
+
+
+// export const contactsApi = () => {
+//   axios.get('/contacts')
+//   .then(res => {
+//     console.log(res.data);
+//   });
+// }
+
+// function getUserAccount() {
+//   return axios.get('http://localhost:3000/');
+// }
+
+// export default getUserAccount;
